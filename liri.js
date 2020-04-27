3d7973c917b692b83eeeb66cc61225907086644f
Terik Hone <terikhone@gmail.com>
2020-04-26 21:40:29 -0600

    searching for concerts api working

---
+++ liri.js
@@ -0,0 +1,78 @@
+
+require("dotenv").config();
+
+const inquirer = require("inquirer");
+const Spotify = require('node-spotify-api');
+const keys = require("./keys.js");
+const axios = require("axios");
+const spotify = new Spotify(keys.spotify);
+
+const liri = {
+
+
+
+  async search(url) {
+    const response = await axios.get(url);
+    //console.log("response: ", response.data);
+    return response.data;
+  },
+
+  concerts() {
+    inquirer.prompt({
+      type: "input",
+      message: "What Band, or Artist would you like to look up?",
+      name: "searchTerm"
+    }).then(async answer => {
+
+      var artist = answer.searchTerm.replace(" ", "+");
+      console.log("search Term: ", artist);
+      const url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+      const result = await this.search(url);
+      console.log("result: ", result);
+    });
+  },
+
+  async music(str1) {
+    const url = "https://rest.bandsintown.com/artists/" + str1 + "/events?app_id=codingbootcamp";
+    const result = await this.search(url);
+    console.log(result)
+  },
+
+  async movies(str1) {
+    const url = "https://rest.bandsintown.com/artists/" + str1 + "/events?app_id=codingbootcamp";
+    const result = await this.search(url);
+    console.log(result)
+  },
+
+  async justDoIt(str1) {
+    const url = "https://rest.bandsintown.com/artists/" + str1 + "/events?app_id=codingbootcamp";
+    const result = await this.search(url);
+    console.log(result)
+  },
+}
+
+
+inquirer.prompt([
+  {
+    type: "list",
+    message: "Please Select one...",
+    choices: [
+      "concert-this",
+      "spotify-this-song",
+      "movie-this",
+      "do-what-it-says"
+    ],
+    name: "searchType"
+  }
+]).then(answer => {
+  console.log(answer.searchType);
+
+  switch (answer.searchType) {
+    case "concert-this":
+      console.log("made it!")
+      liri.concerts();
+  }
+}).catch(error => {
+  console.error(error);
+});
+
